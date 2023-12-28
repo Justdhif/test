19c16ae79fd59cd693c6c73b1246c4688f32682c
Justdhif <noob1234five@gmail.com>
2023-12-28 13:55:53 +0700

    Justdhif

---
+++ Script.js
@@ -0,0 +1,34 @@
+data = [{
+    'img1': 'aset/eme.jpg',
+    'img2': 'aset/eke.jpg',
+    'title':'Mobile legend',
+    'desc':'Untuk project ml mungkin gada ya tapi aku pernah masuk top 1 supreme Granger'
+}, {
+    'img1': 'aset/project.jpg',
+    'img2': 'aset/proyec.jpg',
+    'title':'Lego tower',
+    'subtitle':'lego tower',
+    'desc':'Salah satu project yang pernah ku buat adalah membuat tower dari beberapa block lego yg di satukan'
+}, {
+    'img1': 'aset/pict.jpg',
+    'img2': 'aset/pii.jpg',
+    'title': 'Picsart',
+    'desc': 'Untuk project picsart mungkin kaya foto disamping, bisa di liat sendiri'
+}] //img 3
+
+var container = document.querySelector('.container-project')
+
+data.forEach(element => {
+    container.innerHTML +=
+    `<div class="project">
+    <div class="img-project">
+        <img src="`+element.img1+`" alt="">
+        <img src="${element.img2}" alt="">
+    </div>
+    <div class="desc-project">
+        <h1>${element.title}</h1>
+        <p>${element.desc}</p>
+    </div>
+    <button class="read-more">Read More</button>
+</div>`
+});
